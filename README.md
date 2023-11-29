acac51bb6ca2b5793799d4539ce23bd46208c354
DESKTOP-OEHME15\user <dino.wu@musense.tw>
2023-11-29 10:41:47 +0800

    更新monitor指令與新增readme

---
+++ README.md
@@ -0,0 +1,73 @@
+# 萬事起頭難, 但欲 Bot 不難!
+
+用途：監測 Linux 系統檔案狀況, 與透過 telegram 訊息操控簡易系統指令
+
+### **首先必須先申請一個 Telegram 的 BOT!**
+
+1.在 telegram 裡面找到 @BotFather  
+2.使用/newbot 申請一個新的 Bot  
+3.依照指示完成後續動作後會提供你一個 Bot token  
+4.將 Bot token 放入 env 設定裡面
+
+#### 該在本專案放入哪些`.env` 變數呢?
+
+- [x] **TELEGRAM_TOKEN**: 放入剛剛取得的 Bot token
+- [x] **CHAT_ID**: 通知對象或群組的 CHAT_ID
+- [x] **FILE_PATH**: 需要監控的檔案絕對路徑
+
+#### 預先取得 CHAT_ID 的方式:
+
+1.使用網頁版的 telegram 登入帳號  
+2.點擊想要讓 Bot 通知的對象或群組對話視窗  
+3.網址上方#後面的數字就是該對象的 CHAT_ID
+
+#### 如何啟動?
+
+建議使用`PM2`,提高應用程式的可靠性以及後續管理效率  
+(有關 PM2 的詳細操作, 可以參考https://pm2.keymetrics.io/docs/usage/quick-start/)  
+專案內安裝:
+
+```
+npm i pm2
+```
+
+or  
+環境全局安裝:
+
+```
+npm install pm2 -g
+```
+
+安裝完成之後可以使用下列命令開啟應用程式:
+
+```
+pm2 start monitor.js --name [自定義程序名稱]
+```
+
+#### 可輸入指令:
+
+`/sysinfo`：顯示系統中使用 CPU 與記憶體最多的前 10 個 Process  
+`/syslog`：顯示 syslog 最後 10 行 log  
+`/mongodlog`：顯示 mongod 最後 10 行 log  
+`/checkProcStatus + [Process Name]`：使用/checkProcStatus 加上 Process 名稱, 例如/checkProcStatus mongod 就會顯示目前該執行緒的 VM size 與 rss 值  
+`/restartDB`：重新啟動 MongoDB <span style="color: red;">**(慎用!)**</span>
+
+#### 調整監控檔案與監控訊息
+
+- 修改監控目標檔案：FILE_PATH
+- 修改監控目標訊息（如果在檔案內偵測到目標訊息時即時通知）
+
+```js
+//調整includes字串
+if (newContent.includes("Out of memory:")) {
+ ....
+ ....
+//調整includes字串
+ const alertLine = lines.reverse().find((line) => line.includes("Out of memory:"));
+ ....
+ ....
+}
+```
+
+如果有偵測到目標訊息會顯示下列通知:  
+![BOT通知圖片](https://reurl.cc/y63GYE "telegram alert範例.")
--- monitor.js
+++ monitor.js
@@ -3,16 +3,18 @@ const fs = require("fs");
 const exec = require("child_process").exec;
 require("dotenv").config();
 
-// replace the value below with the Telegram token you received from @BotFather
+// 在ENV內替換各數值
 const token = process.env.TELEGRAM_TOKEN;
 const filePath = process.env.FILE_PATH;
 const chatId = process.env.CHAT_ID;
 
 console.log("bot is monitoring...");
-// Create a bot that uses 'polling' to fetch new updates
+// 使用polling模式讓BOT可以持續接收與更新訊息
 const bot = new TelegramBot(token, { polling: true });
+
 bot.on("message", (msg) => {
   const chatId = msg.chat.id;
+  // 在log中留存確認BOT通知的對象ID
   console.log("Chat ID:", chatId);
   if (msg.text === "/sysinfo") {
     exec("ps aux --sort=-%cpu,-%mem | head -n 11", (error, stdout, stderr) => {
@@ -27,7 +29,7 @@ bot.on("message", (msg) => {
       bot.sendMessage(chatId, `<pre>${stdout}</pre>`, { parse_mode: "HTML" });
     });
   }
-  if (msg.text === "/restart") {
+  if (msg.text === "/restartDB") {
     exec("sudo systemctl restart mongod", (error, stdout, stderr) => {
       if (error) {
         bot.sendMessage(chatId, `Error restarting DB: ${error.message}`);
@@ -41,7 +43,7 @@ bot.on("message", (msg) => {
     });
   }
   if (msg.text === "/syslog") {
-    exec("tail -n 10 /var/log/syslog", (error, stdout, stderr) => {
+    exec("sudo tail -n 10 /var/log/syslog", (error, stdout, stderr) => {
       if (error) {
         bot.sendMessage(chatId, `Error reading syslog: ${error.message}`);
         return;
@@ -55,14 +57,44 @@ bot.on("message", (msg) => {
       });
     });
   }
-  if (msg.text === "/nginxlog") {
-    exec("tail -n 10 /var/log/nginx/error.log", (error, stdout, stderr) => {
+  if (msg.text === "/mongodlog") {
+    exec(
+      "sudo tail -n 10 /var/log/mongodb/mongod.log",
+      (error, stdout, stderr) => {
+        if (error) {
+          bot.sendMessage(chatId, `Error reading mongod log: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          bot.sendMessage(chatId, `mongod log read with warnings: ${stderr}`);
+          return;
+        }
+        bot.sendMessage(chatId, "```\n" + stdout + "\n```", {
+          parse_mode: "MarkdownV2",
+        });
+      }
+    );
+  }
+  // 使用 /checkProcStatus+執行緒名稱檢查狀況, 避免服務重啟時PID不固定的問題
+  if (msg.text.startsWith("/checkProcStatus")) {
+    const procName = msg.text.split(" ")[1];
+
+    if (!procName) {
+      bot.sendMessage(chatId, "Please specify a process name.");
+      return;
+    }
+
+    const cmd = `cat /proc/$(pgrep -x ${procName})/status | grep -E 'VmSize|RssAnon'`;
+    exec(cmd, (error, stdout, stderr) => {
       if (error) {
-        bot.sendMessage(chatId, `Error reading errorlog: ${error.message}`);
+        bot.sendMessage(
+          chatId,
+          `Error reading process status: ${error.message}`
+        );
         return;
       }
       if (stderr) {
-        bot.sendMessage(chatId, `errorlog read with warnings: ${stderr}`);
+        bot.sendMessage(chatId, `Process Status read with warnings: ${stderr}`);
         return;
       }
       bot.sendMessage(chatId, "```\n" + stdout + "\n```", {
@@ -75,21 +107,22 @@ bot.on("message", (msg) => {
 let previousSize = 0;
 
 fs.watchFile(filePath, (curr, prev) => {
+  // 檢查文件是否被修改 (mtime=modification time)
   if (curr.mtime !== prev.mtime) {
-    // File has been modified
     const newSize = curr.size;
     const sizeDiff = newSize - previousSize;
-
+    // 檢查文件內容是否增加
     if (sizeDiff > 0) {
+      // 使用 Buffer 分配新内容大小的空間
       const buffer = Buffer.alloc(sizeDiff);
 
-      // Open the file
+      // 用唯讀的方式開啟檔案
       fs.open(filePath, "r", (err, fd) => {
         if (err) throw err;
 
-        // Read the new content
+        // 從先前文件大小的位置開始讀取，只讀取新增的部分
         fs.read(
-          fd,
+          fd, //file descriptor
           buffer,
           0,
           sizeDiff,
@@ -98,13 +131,11 @@ fs.watchFile(filePath, (curr, prev) => {
             if (err) throw err;
 
             const newContent = buffer.toString("utf8");
-
-            // Check if the new content contains the specified string
+            // 快速檢查新增的内容中是否包含特定字符串
             if (newContent.includes("Out of memory:")) {
-              // Split the new content into lines
               const lines = newContent.split(/\r?\n/);
 
-              // Find the line that contains the specified string
+              // 從最後面開始找出確切包含該字符串的行,確定是最新的內容
               const alertLine = lines
                 .reverse()
                 .find((line) => line.includes("Out of memory:"));
@@ -112,7 +143,7 @@ fs.watchFile(filePath, (curr, prev) => {
                 bot.sendMessage(chatId, `Alert: ${alertLine}`);
               }
             }
-            // Update the previous size
+            // 更新PreviousSize方便下次比較
             previousSize = newSize;
 
             fs.close(fd, (err) => {
@@ -124,6 +155,8 @@ fs.watchFile(filePath, (curr, prev) => {
     }
   }
 });
+
+// 監控錯誤
 bot.on("polling_error", (error) => {
-  console.log(error); // => 'EFATAL'
+  console.log(error);
 });
